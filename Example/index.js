66e9ca79e2c46dbe7740a87ac4ff1433985b8365
Alex <a.matulionis@usa.com>
2019-07-29 20:23:53 -0700

    Added base.

---
+++ Example/index.js
@@ -0,0 +1,4 @@
+import Thread from '../Thread'
+
+let th = new Thread()
+th.Add({ yeah: { man: 123 } })
---
+++ Thread.ts
@@ -0,0 +1,10 @@
+class Thread {
+  constructor() {
+  }
+
+  Add(diff: any) {
+    console.log(`Thread: added ${JSON.stringify(diff)}`)
+  }
+}
+
+export default Thread
