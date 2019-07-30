f0f2da4a56e400e127a53db5e5d863535a3d1c29
Alex <a.matulionis@usa.com>
2019-07-29 21:40:45 -0700

    Added more cases and public methods.

--- Example/Example.js
+++ Example/Example.js
@@ -1,4 +1,26 @@
-import Thread from '../Thread'
+import Thread, { __ } from '../Thread'
 
-let th = new Thread()
-th.Add({ yeah: { man: 123 } })
+let th = new Thread('Alex')
+
+th.Filter(
+  ({ x, y }, a, b) => {
+    return a !== b && !!x && !!y
+  },
+  { coords: __ }
+)
+
+th.On(
+  (v) => {
+    alert(JSON.stringify(v))
+  },
+  { yeah: __ }
+)
+
+th.Connect('Tyler')
+
+th.Add(
+  {
+    yeah: { man: 123 },
+    coords: { x: 2, y: 4 }
+  }
+)
--- Thread.ts
+++ Thread.ts
@@ -1,10 +1,31 @@
 class Thread {
-  constructor() {
-  }
+    private uid: string
 
-  Add(diff: any) {
-    console.log(`Thread: added ${JSON.stringify(diff)}`)
-  }
+    constructor(uid: string) {
+        this.uid = uid
+    }
+
+    async Connect(uid: string) {
+        console.log(`Thread: connected to ${uid}`)
+    }
+
+    On(callback: (v: any) => void, path: any) {
+        console.log(`Thread: waiting on ${JSON.stringify(path)}`)
+        callback({ man: "yo" })
+    }
+
+    Filter(callback: (v: any, uidA: string, uidB: string) => boolean, path: any) {
+        console.log(`Thread: filtering ${JSON.stringify(path)}; ${callback({ x: 10, y: 5 }, 'Alex', 'Tyler')}`)
+    }
+
+    Add(diff: any) {
+        console.log(`Thread: added ${JSON.stringify(diff)}`)
+    }
 }
 
+const __: any = "\u0007"
+
 export default Thread
+export {
+    __
+}
